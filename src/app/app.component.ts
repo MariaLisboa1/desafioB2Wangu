7a5571af748ca5a082093655b8923395428a91c2
zaza.naza95 <zaza.naza95@gmail.com>
2019-07-24 13:55:10 -0300

    desafio finalizado utilizando angular e bootstrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,8 +11,11 @@ export class AppComponent {
   planet: Array<any> = [];
   films: Array<any> = [];
 
-  noMovie: boolean = false;
+  noFilm: string = "";
   error: boolean = false;
+
+  getBtn
+  span
 
   constructor(
     private swService: SwService
@@ -20,10 +23,12 @@ export class AppComponent {
 
   ngOnInit() {
     this.getPlanet();
+    this.getBtn = document.getElementById('btn');
+    this.span = document.getElementById('span');
   }
 
   private getPlanet() {
-    this.swService.getPlanets(this.numAleat()).subscribe(
+    this.swService.getPlanets(this.numRandom()).subscribe(
       res => {
         this.error = false;
 
@@ -32,44 +37,65 @@ export class AppComponent {
             const film = e;
             const numFilm = film.charAt(film.length - 2);
             this.film(numFilm);
-            this.noMovie = true;
+            this.noFilm = "";
           });
 
         } else {
-          this.noMovie = false;
+          this.noFilm = "No Film";
+          this.getBtn.classList.add("btn");
+          this.getBtn.classList.remove("is-loading");
         }
 
         this.planet = res;
       },
-      err => {
-        this.error = true;
-        console.log(`ERROR ==> ${err}`)
-      }
+      err => this.handleError(err)
     )
   }
 
   private film(film) {
-
-
     this.swService.getFilms(film).subscribe(
       res => {
         this.films.push({
           films: res.title
-        })
+        });
+        this.getBtn.classList.add("btn");
+        this.getBtn.classList.remove("is-loading");
       },
-      err => console.log(err)
+      err => this.handleError(err)
+
     )
   }
 
+  private handleError(error) {
+    this.getBtn.classList.add("is-fail");
+    this.span.innerHTML = "ERROR";
+    this.error = true;
+    console.log(`ERROR => ${error}`);
+  }
+
   private next() {
+
+    this.getBtn = document.getElementById('btn');
+    this.getBtn.classList.add("is-loading");
+
     this.films = [];
     this.getPlanet();
   }
 
-  private numAleat() {
+  private numRandom() {
 
-    var aleatorio = Math.floor(Math.random() * 61);
+    let drawn = [];
+    const maxValue = 61;
 
-    return aleatorio;
+    if (drawn.length === maxValue) {
+      alert('No more planets :(');
+    }
+    let suggestion = Math.ceil(Math.random() * maxValue);
+    while (drawn.indexOf(suggestion) >= 0) {
+      suggestion = Math.ceil(Math.random() * maxValue);
+    }
+    drawn.push(suggestion);
+    return suggestion;
   }
+  
 }
